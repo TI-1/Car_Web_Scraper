6856e36c444385f12d1fff2beca8aed6497b1c86
Tobi <tibuoye@yahoo.co.uk>
2024-04-01 21:06:25 +0100

    Added abstract methods

--- Scraper.py
+++ Scraper.py
@@ -1,12 +1,26 @@
+import re
 from abc import ABC, abstractmethod
 
 
 class CarScraper(ABC):
-
+    @property
     @abstractmethod
-    def get_cars(self):
+    def cars(self):
         pass
 
     @abstractmethod
     def scrape(self):
         pass
+
+    @abstractmethod
+    def numberplate_detector(self, image):
+        pass
+
+    @abstractmethod
+    def get_numberplate(self, article):
+        pass
+
+    @staticmethod
+    def is_valid_uk_number_plate(plate):
+        pattern = r'^[A-Z]{2}\d{2}\s?[A-Z]{3}$'
+        return bool(re.match(pattern, plate.upper()))
