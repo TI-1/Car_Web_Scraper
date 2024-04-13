1cd7c646ff0007f87839387e5f3195339bd3dea3
Tobi <tibuoye@yahoo.co.uk>
2024-04-13 18:39:28 +0100

    Add repr method

--- Car.py
+++ Car.py
@@ -4,10 +4,15 @@ from typing import List
 
 @dataclass(slots=True)
 class Car:
-    name: str
+    def __repr__(self):
+        return (f"{{Name: {self.listing_name}, NumberPlate: {self.potential_numberplate}, Price: £{self.price}, "
+                f"Year: {self.year}, Mileage: {self.mileage}}}")
+    listing_name: str
+    car_name: str
     url: str
     price: int
     year: int
     potential_numberplate: List[str]
     mileage: int
     owners: int
+
