bbcb91572cf79d603aa9e2b11f00ccf87e252788
Tobi <tibuoye@yahoo.co.uk>
2024-04-13 18:39:57 +0100

    Intial Commit

---
+++ car_dictionary.py
@@ -0,0 +1,175 @@
+# Ford_Dict = {"Ford": {"Model": ["Fiesta", "Focus", "Galaxy", "Mondeo", "Puma", "s-Max"],
+#                       "Model Variant": ["2.3 EcoBoost", "5.0 V8", "Active", "Active X", "Bullitt", "Double Cab",
+#                                         "Edge",
+#                                         "Raptor", "Regular Cab", "Sport", "ST", "ST-Line", "ST-Line X", "Studio",
+#                                         "Style", "Super Cab",
+#                                         "Titanium", "Titanium x", "Trend", "Vignale", "Zetec"]}
+#              }
+#
+# Vauxhaul_Dict = {"Vauxhall": {
+#     "Model": ["Agila", "Astra", "Corsa", "Crossland", "Insignia", "Meriva", "Mokka", "Mokka X", "Grandland",
+#               "Zafira"],
+#     "Model Variant": ["Active", "Business Nav", "Design", "Elite", "Elite Nav", "Elite "
+#                                                                                 "Nav "
+#                                                                                 "Premium", "ES",
+#                       "Griffin", "Griffin Special Edition", "Launch Edition", "SE",
+#                       "SRi Nav", "SRi VX-Line Nav", "Tech Line",
+#                       "Ultimate Nav"]}}
+#
+# Audi_Dict = {"Audi": {
+#     "Model": ["A1", "A3", "A4", "A5", "A6", "A7", "A8", "TT"],
+#     "Model Variant": ["SE", "SE Technik", "S Line", "S1", "S3", "S4", "S5", "Sport", "Sport Edition", "Sport Nav",
+#                       "Technik"]}}
+#
+# BMW_Dict = {"BMW": {
+#     "Model": ["1-Series", "2-Series", "3-Series", "4-Series", "5-Series", "6-Series", "7-Series", "8-Series"],
+#     "Model Variant": ["118i", "118d", "Modern", "M Sport", "SE", "SE Business", "SE Pro", "Sport", "Sport Pro"]
+# }}
+#
+# Citroen_Dict = {"Citroen": {
+#     "Model": ["C1", "C2", "C3", "C4", "C5", "DS3", "DS4"],
+#     "Model Variant": ["Edition", "Exclusive", "Platinum", "Selection", "VT", "VTR+"]
+# }}
+#
+# Fiat_Dict = {"Fiat": {
+#     "Model": ["Fiat 500", "Fiat Grande Punto", "Fiat Punto Evo"],
+#     "Model Variant": []
+# }
+# }
+#
+# Honda_Dict = {"Honda": {
+#     "Model": ["Accord", "Civic", "FR-V", "Jazz", "Stream"]
+# }}
+#
+# Hyundai_Dict = {"Hyundai": {
+#     "Model": ["Getz", "i10", "i20", "i30", "i40", "Veloster"]
+# }}
+#
+# Kia_Dict = {"Kia": {
+#     "Model": ["Ceed", "Cerato", "Niro", "Optima", "Picanto", "ProCeed", "Rio", "Sportage", "Venga"]
+# }}
+#
+# Mercedes_Dict = {"Mercedes": {
+#     "Model": ["A-Class", "C-Class", "B-Class", "CLA", "CLC", "CLR", "E-Class"]
+# }}
+#
+# Mini_Dict = {"MINI": {
+#     "Model": ["Clubman", "Convertible", "Cooper", "Cooper S", "Countryman", "Coupe", "Hatchback"]
+# }}
+#
+# Nissan_Dict = {"Nissan": {
+#     "Model": ["Juke", "Micra", "Note", "Pixo", "Pulsar", "Qashqai"]
+# }}
+#
+# Peugeot_Dict = {"Peugeot": {
+#     "Model": ["107", "108", "207", "208", "307", "308"]
+# }}
+#
+# Renault_Dict = {"Renault": {
+#     "Model": ["Clio", "Captur", "Kadjar", "Megane", "Modus", "Scenic", "Twingo"]
+# }}
+#
+# SEAT_Dict = {"SEAT": {
+#     "Model": ["Altea", "Ateca", "Ibiza", "Leon", "Mii", "Tarraco"]
+# }}
+#
+# Skoda_Dict = {"Skoda": {
+#     "Model": ["Citigo", "Fabia", "Octavia", "Scala", "Superb", "Yet"]
+# }}
+#
+# Suzuki_Dict = {"Suzuki": {
+#     "Model": ["Alto", "Celerio", "Swift"]
+# }}
+#
+# Toyota_Dict = {"Toyota": {
+#     "Model": ["Auris", "Avensis", "Aygo", "Camry", "Corolla", "IQ", "Prius", "Verso", "Yaris"]
+# }}
+#
+# Volkswagen_Dict = {"Volkswagen": {
+#     "Model": ["CC", "Fox", "Golf", "Lupo", "Passat", "Polo", "Scirocco", "Tiguan", "Touareg", "Up"]
+# }}
+#
+# Volvo_Dict = {"Volvo": {
+#     "Model": ["C30", "C40", "C70", "S40", "S60", "S80", "S90", "V40", "V50", "V60", "V70", "V90", "XC40", "XC60"]
+# }}
+
+Car_Dict = {"Ford": {"Model": ["Fiesta", "Focus", "Galaxy", "Mondeo", "Puma", "s-Max"],
+                     "Model Variant": ["2.3 EcoBoost", "5.0 V8", "Active", "Active X", "Bullitt", "Double Cab",
+                                       "Edge",
+                                       "Raptor", "Regular Cab", "Sport", "ST", "ST-Line", "ST-Line X", "Studio",
+                                       "Style", "Super Cab",
+                                       "Titanium", "Titanium x", "Trend", "Vignale", "Zetec"]},
+            "Vauxhall": {
+                "Model": ["Agila", "Astra", "Corsa", "Crossland", "Insignia", "Meriva", "Mokka", "Mokka X", "Grandland",
+                          "Zafira"],
+                "Model Variant": ["Active", "Business Nav", "Design", "Elite", "Elite Nav", "Elite "
+                                                                                            "Nav "
+                                                                                            "Premium", "ES",
+                                  "Griffin", "Griffin Special Edition", "Launch Edition", "SE",
+                                  "SRi Nav", "SRi VX-Line Nav", "Tech Line",
+                                  "Ultimate Nav"]},
+            "Audi": {
+                "Model": ["A1", "A3", "A4", "A5", "A6", "A7", "A8", "TT"],
+                "Model Variant": ["SE", "SE Technik", "S Line", "S1", "S3", "S4", "S5", "Sport", "Sport Edition",
+                                  "Sport Nav",
+                                  "Technik"]},
+            "BMW": {
+                "Model": ["1-Series", "2-Series", "3-Series", "4-Series", "5-Series", "6-Series", "7-Series",
+                          "8-Series", "1 Series", "2 Series", "3 Series", "4 Series", "5 Series", "6 Series",
+                          "7 Series",
+                          "8 Series"],
+                "Model Variant": ["118i", "118d", "Modern", "M Sport", "SE", "SE Business", "SE Pro", "Sport",
+                                  "Sport Pro"]
+            },
+            "Citroen": {
+                "Model": ["C1", "C2", "C3", "C4", "C5", "DS3", "DS4"],
+                "Model Variant": ["Edition", "Exclusive", "Platinum", "Selection", "VT", "VTR+"]
+            },
+            "Fiat": {
+                "Model": ["500", "Grande Punto", "Punto Evo"],
+                "Model Variant": []
+            },
+            "Honda": {
+                "Model": ["Accord", "Civic", "FR-V", "Insight", "Jazz", "Stream"]
+            },
+            "Hyundai": {
+                "Model": ["Getz", "i10", "i20", "i30", "i40", "Veloster"]
+            },
+            "Kia": {
+                "Model": ["Ceed", "Cerato", "Niro", "Optima", "Picanto", "ProCeed", "Rio", "Sportage", "Venga"]
+            },
+            "Mercedes": {
+                "Model": ["A-Class", "C-Class", "B-Class", "CLA", "CLC", "CLR", "E-Class"]
+            },
+            "MINI": {
+                "Model": ["Clubman", "Convertible", "Cooper", "Cooper S", "Countryman", "Coupe", "Hatchback"]
+            },
+            "Nissan": {
+                "Model": ["Juke", "Micra", "Note", "Pixo", "Pulsar", "Qashqai"]
+            },
+            "Peugeot": {
+                "Model": ["107", "108", "207", "208", "307", "308"]
+            },
+            "Renault": {
+                "Model": ["Clio", "Captur", "Kadjar", "Megane", "Modus", "Scenic", "Twingo"]
+            },
+            "SEAT": {
+                "Model": ["Altea", "Ateca", "Ibiza", "Leon", "Mii", "Tarraco"]
+            },
+            "Skoda": {
+                "Model": ["Citigo", "Fabia", "Octavia", "Scala", "Superb", "Yet"]
+            },
+            "Suzuki": {
+                "Model": ["Alto", "Celerio", "Swift"]
+            },
+            "Toyota": {
+                "Model": ["Auris", "Avensis", "Aygo", "Camry", "Corolla", "IQ", "Prius", "Verso", "Yaris"]
+            },
+            "Volkswagen": {
+                "Model": ["CC", "Fox", "Golf", "Lupo", "Passat", "Polo", "Scirocco", "Tiguan", "Touareg", "Up"]
+            },
+            "Volvo": {
+                "Model": ["C30", "C40", "C70", "S40", "S60", "S80", "S90", "V40", "V50", "V60", "V70", "V90", "XC40",
+                          "XC60"]
+            }
+            }
